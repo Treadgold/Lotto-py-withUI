47f720fe575787a26457e61bf999b17356e8a7db
Treadgold <laptoprecording@gmail.com>
2023-02-13 19:07:01 +1300

    formatting

--- UI.py
+++ UI.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 import newlotto
 
+
 class App:
     def __init__(self, master: ctk.CTk):
         self.master = master
@@ -15,90 +16,121 @@ class App:
         slider_pos = .3
         label_font = ("Arial", 14, "bold")
 
-        #create the rows number label
-        self.rows_label = ctk.CTkLabel(self.master, text=f"number of rows {self.row_number}", font = label_font)
+        # create the rows number label
+        self.rows_label = ctk.CTkLabel(
+            self.master,
+            text=f"number of rows {self.row_number}",
+            font=label_font)
         self.rows_label.place(relx=slider_pos, rely=0.1, anchor="e")
-        #Create the rows slider
-        slider_rows = ctk.CTkSlider(self.master, from_=1, to=30, number_of_steps=29, command=self.slider_rows_event)
+        # Create the rows slider
+        slider_rows = ctk.CTkSlider(self.master,
+                                    from_=1,
+                                    to=30,
+                                    number_of_steps=29,
+                                    command=self.slider_rows_event)
         slider_rows.place(relx=slider_pos, rely=0.2, anchor="e")
         slider_rows.set(int(self.row_number))
 
-
-        #create the number of balls drawn label
-        self.balls_drawn_label = ctk.CTkLabel(self.master, text=f"balls to draw {self.balls_drawn}", font = label_font)
+        # create the number of balls drawn label
+        self.balls_drawn_label = ctk.CTkLabel(
+            self.master,
+            text=f"balls to draw {self.balls_drawn}",
+            font=label_font)
         self.balls_drawn_label.place(relx=slider_pos, rely=0.3, anchor="e")
-        #Create the number of balls drawn slider
-        slider_balls_drawn = ctk.CTkSlider(self.master, from_=4, to=9, number_of_steps=5, command=self.slider_balls_drawn_event)
+        # Create the number of balls drawn slider
+        slider_balls_drawn = ctk.CTkSlider(
+            self.master,
+            from_=4,
+            to=9,
+            number_of_steps=5,
+            command=self.slider_balls_drawn_event)
         slider_balls_drawn.place(relx=slider_pos, rely=0.4, anchor="e")
         slider_balls_drawn.set(int(self.balls_drawn))
 
-
-
-        #create the max ball number label
-        self.max_ball_number_label = ctk.CTkLabel(self.master, text=f"max ball number {self.max_ball_number}", font = label_font)
+        # create the max ball number label
+        self.max_ball_number_label = ctk.CTkLabel(
+            self.master,
+            text=f"max ball number {self.max_ball_number}",
+            font=label_font)
         self.max_ball_number_label.place(relx=slider_pos, rely=0.5, anchor="e")
-        #Create the max ball number slider
-        slider_max_ball_number = ctk.CTkSlider(self.master, from_=12, to=99, number_of_steps=87, command=self.slider_max_ball_number_event)
+        # Create the max ball number slider
+        slider_max_ball_number = ctk.CTkSlider(
+            self.master,
+            from_=12,
+            to=99,
+            number_of_steps=87,
+            command=self.slider_max_ball_number_event)
         slider_max_ball_number.place(relx=slider_pos, rely=0.6, anchor="e")
         slider_max_ball_number.set(int(self.max_ball_number))
-        
 
-        #create the powerball label
-        self.powerball_label = ctk.CTkLabel(self.master, text=f"powerball max {self.powerball}", font = label_font)
+        # create the powerball label
+        self.powerball_label = ctk.CTkLabel(
+            self.master,
+            text=f"powerball max {self.powerball}",
+            font=label_font)
         self.powerball_label.place(relx=slider_pos, rely=0.7, anchor="e")
-        #Create the powerball slider
-        slider_powerball = ctk.CTkSlider(self.master, from_=0, to=99, number_of_steps=100, command=self.slider_powerball_event)
+        # Create the powerball slider
+        slider_powerball = ctk.CTkSlider(self.master,
+                                         from_=0,
+                                         to=99,
+                                         number_of_steps=100,
+                                         command=self.slider_powerball_event)
         slider_powerball.place(relx=slider_pos, rely=0.8, anchor="e")
         slider_powerball.set(int(self.powerball))
-        
-        
-        #Create the button
-        self.button = ctk.CTkButton(self.master, text="Generate Numbers", command=self.populate_box, corner_radius=10, font = label_font)
+
+        # Create the button
+        self.button = ctk.CTkButton(self.master,
+                                    text="Generate Numbers",
+                                    command=self.populate_box,
+                                    corner_radius=10,
+                                    font=label_font)
         self.button.place(relx=slider_pos, rely=0.9, anchor="e")
 
-        #create the text box
-        self.text_box = ctk.CTkTextbox(self.master, border_spacing=25, width=450, height=500, font=("Courier bold", 23), text_color = 'Black', corner_radius = 10, fg_color = 'grey')
+        # create the text box
+        self.text_box = ctk.CTkTextbox(self.master,
+                                       border_spacing=25,
+                                       width=450,
+                                       height=500,
+                                       font=("Courier bold", 23),
+                                       text_color='Black',
+                                       corner_radius=10,
+                                       fg_color='grey')
         self.text_box.place(relx=0.98, rely=0.5, anchor="e")
-    
+
     def slider_rows_event(self, value):
         self.row_number = str(int(value))
         self.rows_label.configure(text=f"number of rows {self.row_number}")
-        
 
     def slider_balls_drawn_event(self, value):
         self.balls_drawn = str(int(value))
-        self.balls_drawn_label.configure(text=f"balls to draw {self.balls_drawn}")
-               
+        self.balls_drawn_label.configure(
+            text=f"balls to draw {self.balls_drawn}")
 
-        
     def slider_max_ball_number_event(self, value):
         self.max_ball_number = str(int(value))
-        self.max_ball_number_label.configure(text=f"max ball number {self.max_ball_number}")
-         
+        self.max_ball_number_label.configure(
+            text=f"max ball number {self.max_ball_number}")
 
     def slider_powerball_event(self, value):
         self.powerball = str(int(value))
         self.powerball_label.configure(text=f"powerball max {self.powerball}")
-              
 
     def populate_box(self):
         self.text_box.delete("1.0", "100.0")
-        
+
         # Get the figures from the sliders
-        final_lines = newlotto.get_lines(
-            int(self.row_number),
-            int(self.max_ball_number),
-            int(self.balls_drawn),
-            int(self.powerball)
-            )
+        final_lines = newlotto.get_lines(int(self.row_number),
+                                         int(self.max_ball_number),
+                                         int(self.balls_drawn),
+                                         int(self.powerball))
 
         # loop through the lines and add them to the text box
-        
+
         for y, line in enumerate(final_lines):
             # check result as tuple will have powerball, else just normal line
 
             if type(line) is tuple:
-                
+
                 final_line = ""
                 j = ", "
                 # loop through the list and add the comma
@@ -109,10 +141,10 @@ class App:
                     final_line += item + j
                 # add the powerball to the end of the line
                 final_line += ", (" + line[1] + ")"
-                
+
                 # add the line to the text box
                 self.text_box.insert("0.0", final_line + "\n")
-                
+
             else:
                 # same as above, just no powerball so no tuple
                 final_line = ""
@@ -123,7 +155,6 @@ class App:
                     final_line += item + j
                 self.text_box.insert("0.0", final_line + "\n")
         pass
-    
 
 
 if __name__ == '__main__':
--- newlotto.py
+++ newlotto.py
@@ -4,19 +4,17 @@ import os
 
 def clear_console():
     # clear the console
-    
-    #check if the os is windows
+
+    # check if the os is windows
     if os.name == 'nt':
         os.system('cls')
-    #otherwise assume it is linux
+    # otherwise assume it is linux
     else:
         os.system('clear')
-    
 
-def get_numbers(max_value = 40,
-                powerball = 10,
-                number_of_balls_drawn = 6):
-        
+
+def get_numbers(max_value=40, powerball=10, number_of_balls_drawn=6):
+
     # make list of numbers for main draw
     balls = [str(i).zfill(2) for i in range(1, max_value + 1)]
 
@@ -26,35 +24,31 @@ def get_numbers(max_value = 40,
 
     # shuffle the balls
     random.shuffle(balls)
-    
+
     # take a sample of the balls
     # Sort them so they are in order
     drawn_balls = sorted((balls[:number_of_balls_drawn]))
-    
+
     # shuffle the powerball
     if powerball != 0:
         random.shuffle(powerball)
         drawn_powerball = powerball[0]
         # if powerball is not 0, return both the main draw and the powerball
         # as a tuple
-        
+
         final = (drawn_balls, drawn_powerball)
         return final
 
-    #otherwise return just the main draw
+    # otherwise return just the main draw
     return drawn_balls
 
 
-def get_lines(lines = 6, max_value = 40, number_of_balls_drawn = 6, powerball=10):
+def get_lines(lines=6, max_value=40, number_of_balls_drawn=6, powerball=10):
     # Generate a set of numbers for the number of lines requested
     # does this by calling get_numbers() and appending the result to a list
-    
+
     all_sets = []
     while len(all_sets) < lines:
-        all_sets.append(get_numbers(max_value, powerball, number_of_balls_drawn))
+        all_sets.append(
+            get_numbers(max_value, powerball, number_of_balls_drawn))
     return all_sets
-
-
-
-
-
